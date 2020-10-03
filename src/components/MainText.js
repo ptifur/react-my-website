d14d1f9888dd907799006ea974753c167755456f
Y <ptifur@gmail.com>
2020-10-03 16:03:45 +0300

    react practice

---
+++ src/components/MainText.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainText = () => {
+    return (
+        <div className='maintext'>
+            <h1>Oh, hi there!</h1>
+            <p>My name is Yar. I'm making my first steps in web development. Here is my playground.</p>
+            <p>This <a href="https://type.ptifur.digital" className='inline' target="_blank" rel="noopener noreferrer">project</a> lets you practice typing letters.</p>
+            
+        </div>
+    )
+}
+
+export default MainText
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='title'>PtiFur</div>
+            <div className='menu'>
+                <div><a href="http://twitter.com/ptifurr" target="_blank" rel="noopener noreferrer">twitter</a></div>
+                <div><a href="https://github.com/ptifur" target="_blank" rel="noopener noreferrer">source code</a></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
