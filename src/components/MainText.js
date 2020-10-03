6fb56c08a6933e016e20536c7774412dc96095b0
Y <ptifur@gmail.com>
2020-10-03 17:24:32 +0300

    react practice

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -5,7 +5,7 @@ const MainText = () => {
         <div className='maintext'>
             <h1>Oh, hi there!</h1>
             <p>My name is Yar. I'm making my first steps in web development. Here is my playground</p>
-            <p><a href="https://type.ptifur.digital" className='inline' target="_blank" rel="noopener noreferrer">PtiType</a> is my JavaScript project. It lets you practice typing 🎮</p>
+            <p><a href="https://type.ptifur.digital" className='inline' target="_blank" rel="noopener noreferrer">PtiType</a> is my JavaScript project. It lets you practice typing</p>
         
         </div>
     )
