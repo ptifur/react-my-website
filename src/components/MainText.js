947aff7423196175ad79dc56b2c634bcd6a5ebdb
Y <ptifur@gmail.com>
2020-10-04 08:50:59 +0300

    updated margins

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -5,7 +5,7 @@ const MainText = () => {
         <div className='maintext'>
             <h1>Hi there!</h1>
             <p>My name is Yar. Making first steps in web development.</p>
-            <p>My plan for the nearest month is to figure out how React works. And contribute to the real project!</p>
+            <p>The plan is to figure out how React works. And contribute to the real project!</p>
             <p>
             You may reach me at <a href="mailto:ptifur@gmail.com" target="_blank" rel="noopener noreferrer" className="inline">ptifur@gmail.com</a> or follow me on <a href="http://twitter.com/ptifurr" target="_blank" rel="noopener noreferrer" className='inline'>twitter</a>.</p>
 
