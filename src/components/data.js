834cf0ae9a5d9c7fd241de0058973ced63719632
Y <ptifur@gmail.com>
2020-11-04 00:10:21 +0200

    updated blog posts text

--- src/components/data.js
+++ src/components/data.js
@@ -1,20 +1,20 @@
 export const postData = [
     {
         "id": 1,
-        "date": "Oct 3, 2020",
+        "date": "Oct, 2020",
         "title": "This website",
         "text": "<a href='https://ptifur.digital' target='_blank' rel='noopener noreferrer'>ptifur.digital</a> is actually a React practice project itself"
     },
     {
         "id": 2,
-        "date": "Oct 4, 2020",
+        "date": "Aug, 2020",
         "title": "Previous version with the red blob",
         "text": "<a href='https://prev.ptifur.digital' target='_blank' rel='noopener noreferrer'>prev.ptifur.digital</a> HTML, CSS responsive website"
     },
     {
         "id": 3,
-        "date": "Oct 4, 2020",
+        "date": "Oct, 2020",
         "title": "PtiType",
-        "text": "<a href='https://type.ptifur.digital' target='_blank' rel='noopener noreferrer'>PtiType</a> JavaScript practice project. It lets you practice typing"
+        "text": "<a href='https://type.ptifur.digital' target='_blank' rel='noopener noreferrer'>type.ptifur.digital</a> JavaScript practice project. It lets you practice typing"
     }
 ]
