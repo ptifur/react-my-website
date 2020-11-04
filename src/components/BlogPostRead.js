81b4b6f031e6795c0d92ed36d9a6db506d8b22b0
Y <ptifur@gmail.com>
2020-11-04 06:19:22 +0200

    created data folder

--- src/components/BlogPostRead.js
+++ src/components/BlogPostRead.js
@@ -2,29 +2,21 @@ import React from 'react'
 import { postData } from './data.js'
 
 const BlogPostRead = () => {
-
     // console.log(postData[1].text)
-    
     return (
         <>
-
             <h2>My projects so far</h2>
 
             { postData.map((data, key) => {
-
-                // console.log(data.text)
 
                 return (
                     <div key={key} className='blogpost'>
                         <p className='date'>{data.date}</p>
                         <h3>{data.title}</h3>
-                        
                         <div dangerouslySetInnerHTML={{__html: data.text }} />
-
                     </div>
                 )
             })}
-
         </>
     )
 }
