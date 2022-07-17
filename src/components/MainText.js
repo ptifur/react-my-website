59b2b735a62f9b4909138dc4fff01b5aafea9136
Y <my@email.com>
2022-07-17 20:18:57 +0300

    Updated text

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -5,9 +5,13 @@ const MainText = () => {
         <div className='maintext'>
             <h1>Hi there!</h1>
             <p>My name is Yar. Making first steps in web development.</p>
-            <p>The plan is to figure out how React works. And contribute to the real project!</p>
+            {/* <p>The plan is to figure out how React works. And contribute to the real project!</p> */}
             <p>
-            You may reach me at <a href="mailto:ptifur@gmail.com" target="_blank" rel="noopener noreferrer" className="inline">ptifur@gmail.com</a> or follow me on <a href="http://twitter.com/ptifurr" target="_blank" rel="noopener noreferrer" className='inline'>twitter</a>.</p>
+            You may reach me at <a href="mailto:ptifur@gmail.com" target="_blank" rel="noopener noreferrer" className="inline">ptifur@gmail.com</a> 
+            
+            {/* or follow me on <a href="http://twitter.com/ptifurr" target="_blank" rel="noopener noreferrer" className='inline'>twitter</a>. */}
+            
+            </p>
 
             {/* <p><a href="https://type.ptifur.digital" className='inline' target="_blank" rel="noopener noreferrer">PtiType</a> is a JavaScript project. It lets you practice typing  <span role='img' aria-labelledby="joystick" className='emoji'>🎮</span></p> */}
         </div>
